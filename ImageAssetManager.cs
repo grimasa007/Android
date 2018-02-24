98ccc37c38cf7db087dbde829d9428c38814f748
Yuri Zolotarev <yzolotarev@gmail.com>
2018-02-24 15:15:41 +0300

    Updated Image Cacher for Android

--- ImageAssetManager.cs
+++ ImageAssetManager.cs
@@ -1,4 +1,19 @@
- public static class ImageAssetManager
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Tester.Helpers
+{
+    public static class ImageAssetManager
     {
         static Dictionary<string, Drawable> cache = new Dictionary<string, Drawable>();
 
@@ -14,3 +29,4 @@
             return cache[url];
         }
     }
+}
